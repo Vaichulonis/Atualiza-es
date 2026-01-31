3491e19025f8af4f22d503875396cb8e9ee38a5c
Vaichulonis <diegoliamvaichulonis@gmail.com>
2026-01-31 15:25:33 -0300

    Update Bot_ecológico.py
    Mudei a barra "/" para um cifrão "$" para não dar conflito com os comandos do discord.

--- Bot_ecológico.py
+++ Bot_ecológico.py
@@ -1,7 +1,7 @@
 #Importações
 import discord
 
-comandos = {"/help", "/what", "/cause", "/problems", "/do"}
+comandos = {"$help", "$what", "$cause", "$problems", "$do"}
 
 # A variável intents armazena as permissões do bot
 intents = discord.Intents.default()
@@ -20,19 +20,19 @@ async def on_message(message):
         return
 
   #Comandos
-    if message.content.startswith('/help'):
+    if message.content.startswith('$help'):
         await message.channel.send("Esses são os comandos disponíveis no momento:", comandos)
     
-    elif message.content.startswith("/what"):
+    elif message.content.startswith("$what"):
         await message.channel.send("A poluição é a introdução de coisas que degradam o meio ambiente e prejudicam a saúde humana.")
 
-    elif message.content.startswith("/cause"):
+    elif message.content.startswith("$cause"):
         await message.channel.send("A poluição pode causar problemas respiratórios, alergias, dores de cabeça e fadiga.")
     
-    elif message.content.startswith("/problems"):
+    elif message.content.startswith("$problems"):
         await message.channel.send("Dentre os principais problemas estão: produtos de limpeza químicos, mofo, poeira, fumaça e lixos.")
 
-    elif message.content.startswith("/do"):
+    elif message.content.startswith("$do"):
         await message.channel.send("Alguns dos cuidados que você pode ters são: jogar o lixo no lixo, lavar a louça após as refeições, tirar as fezes dos seus animais (se tiver), limpar a casa.")
 
 client.run("Bote o seu totem aqui")
